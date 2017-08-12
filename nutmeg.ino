6c78bfb90858b49533eb373f233f1ac10880ab69
ner0tic <david.durost@gmail.com>
2017-08-12 15:16:57 -0400

    Code cleanupp and simplification

--- nutmeg.ino
+++ nutmeg.ino
@@ -1,148 +1,141 @@
 /*
   RaggedPi Project
-  Arduino 2 "Nutmeg" - Circulation Fan
+  Arduino "Nutmeg" - Circulation Fan
   Written by david durost <david.durost@gmail.com>
 */
+/* Includes */
 #include <SoftwareSerial.h>     // Serial library
-#include <TimeLib.h>            // Time library
-#include <Wire.h>               // One Wire library
-#include <DS1307RTC.h>          // Realtime Clock library
-#include <SPI.h>                // SPI library
-#include <SD.h>                 // SD library
 #include <Relay.h>              // Relay library
-#include <DHT.h>
-extern "C" {
-    #include <Utility.h>
+#include <DHT.h>                // Thermometer library
+
+/* Misc Constants */
+#define SDA 2                   // SDA
+#define SDL 3                   // SDL
+#define CS 10                   // Chipselect
+#define LED 13                  // LED pin
+#define DHT_PIN 5               // Analog pin
+#define DHT_F true              // Use fahrenheit
+#define TEMP_SETTING 62         // Temp setting to trigger relay in F
+#define SLEEP_TIME 90000        // ms
+
+/* Objects */
+Relay fanRelay(RELAY4, LOW);
+DHT dht(DHT_PIN, DHT11);
+
+/* Enums */
+enum Modes {
+    MONITOR_MODE,
+    SLEEP_MODE,
+    MANUAL_MODE
+};
+
+/* Variables */
+unsigned long sleep = 0;
+Modes status = MONITOR_MODE;
+bool override = false;
+float temp = 32.00;
+
+/**
+ * Monitor mode
+ */
+void monitorMode() {
+    // If relay is off and temp rises above threshold
+    if (!override && fanRelay.isOff() && temp >= TEMP_SETTING)  {
+        Serial.print("Temp: ");
+        Serial.print(temp);
+        Serial.println(" F\tFan Activated.");
+        fanRelay.on();
+    } 
+    // Else if relay is on and temp falls below threshold
+    else if (!override && fanRelay.isOn() && temp < TEMP_SETTING) {
+        Serial.print("Temp: ");
+        Serial.print(temp);
+        Serial.println(" F\tFan Deactivated.");
+        fanRelay.off();
+        status = SLEEP_MODE;
+        sleep = millis();
+    } 
+    // Else if override is true
+    else if (override) {
+        fanRelay.toggle();
+        if (fanRelay.isOff()) {
+            status = SLEEP_MODE;
+            sleep = millis();
+        } else {
+            Serial.println("Fan manually activated.");
+        }
+    }
 }
 
-// Debugging
-// #define DEBUG                   // uncomment for debugger
-//#define XBEE                    // uncomment to enable xbee transmission
-#define LOG                     // uncomment to enable datalogging
-#define SERIAL                  // uncomment to enable serial output
-// Thermometer
-#define DHTPIN 5                // digital pin
-#define DHTFAHRENHEIT true      // Fahrenheit (true) or Celsius (false)
-#define TEMP 58                 // Temperature trigger (Fahrenheit)
+/**
+ * Sleep mode
+ */
+void sleepMode() {
+    long sleeptime = millis() - sleep;
+    fanRelay.off();
+    Serial.println("Sleeping...");
 
-// Variables
-Relay fanRelay(RELAY1, LOW);    // relay
-DHT dht(DHTPIN, DHT11);         // dht11
-#ifdef LOG
-File logfile;                   // log file
-#endif
-#ifdef XBEE
-SoftwareSerial XBee(SDA, SDL);  // XBee2
-#endif
-//char* msg;                      // reused variable
-//tmElements_t rtc;               // RTC
+    if (sleeptime > SLEEP_TIME) {
+        status = MONITOR_MODE;
+    }
+}
 
+/**
+ * Manual mode
+ */
+void manualMode() {
+    override = true;
+    status = MONITOR_MODE;
+}
 
 /**
  * Setup
  */
 void setup() {
-    bool parse=false;
-    bool config=false;
-
     Serial.begin(9600);
     while (!Serial);
 
-    #ifdef XBEE
-    XBee.begin(9600);
-    #endif
-    print("RaggedPi Project Codename Nutmeg Initializing");
-    dotdotdot();
-    println();
-    
-    // SPI
-    SPI.begin();
-
-    // RTC 
-    print("Initializing RTC");
-    dotdotdot(5);
-    
-    if (setDateFromString(__DATE__) && setTimeFromString(__TIME__)) {
-        parse = true;
-        if (RTC.write(rtc)) {
-            config = true;
-        }
-    }
-    delay(200);
-    if (parse && config) {
-    } else if (parse) {
-        println("Failed");
-        error("DS1307 Communication Error");
-    } else {
-        println("Failed");
-        sprintf(msg, "Could not parse info from the compiler, Time=\"%s\", Date=\"%s\"", __TIME__, __DATE__);
-        error(msg);
-    }
-    println("Initialized.");
-
-    // SD
-    print("Initializing SD card");
-    dotdotdot(10, ".", 100, true);
-    if (!SD.begin(CS))  error("SD card failed or is missing");
-    println("Initialized.");
-
-    // Logging
-    #ifdef LOG
-    char filename[] = "LOG00.csv";
-    for (uint8_t i = 0; i < 100; i++) {
-        filename[3] = i / 10 + '0';
-        filename[4] = i % 10 + '0';
-        if (!SD.exists(filename)) {
-            logfile = SD.open(filename, FILE_WRITE);
-            break;
-        }
-    }
-    if (!logfile) {
-        error("log file creation failed.  System will continue.", false);
-        #undef LOG
-    }
-    else {
-        sprintf(msg, "Logfile: %s", filename);
-        println(msg);
-    }
-    #endif
-
-    // 1 WIRE
-    Wire.begin();
-
-    // Relay
-    fanRelay.begin();
-
-    // Set pins
-    pinMode(DHTPIN, INPUT);
+    /* Set pins ***************************************************************/
+    pinMode(DHT_PIN, INPUT);
     pinMode(LED, OUTPUT);
     pinMode(CS, OUTPUT);
-    println("System initialized.");
+
+    Serial.println("RaggedPi Project Codename Nutmeg Initializing...");
+    
+    /* Relay ******************************************************************/
+    Serial.print("Initializing relays...");
+    fanRelay.begin();
+    Serial.println("[OK]");
+    delay(600);
+    Serial.print("Initializing dht...");
+    dht.begin();
+    Serial.println("[OK]");
+    delay(600);
+    Serial.println("System initialized.");
+    delay(600);
 }
 
-/**
- * Loop
- */
 void loop() {
-    RTC.read(rtc);
-
-    // Read DHT data (~250ms)
-    float temp = dht.readTemperature(DHTFAHRENHEIT);
-    if (isnan(temp))   warning("Failed to read from DHT sensor! (t)");   
-
-    // Fan relay
-    if ((TEMP == temp && fanRelay.isOff()) || (TEMP == temp && fanRelay.isOn())) {
-        fanRelay.toggle();
-        sprintf(msg, "%s [%s] - fan", getDateTimeString(), temp);
-        if (fanRelay.isOn()) {
-            sprintf(msg,"%s %s", msg, "activated");
-            startTimer();
-        } else {
-            stopTimer();
-            sprintf(msg, "%s %s [%s]", msg, "deactivated", calculateTimerResults(endTime - startTime));
-        }
-        println(msg);
-        delay(3000);
-        logfile.close();
+    // Read dht
+    temp = dht.readTemperature(DHT_F);
+    // Verify reading
+    if (isnan(temp)) {
+        fanRelay.on();
+        Serial.print("[ERROR] Failed to read from DHT sensor.  Fan will remain on.");   
+        override = true;
     }
+    
+    switch (status) {
+        case MONITOR_MODE:
+            monitorMode();
+            break;
+        case SLEEP_MODE:
+            sleepMode();
+            break;
+        case MANUAL_MODE:
+            manualMode();
+            break;
+    }
+
+    delay(3000);
 }
